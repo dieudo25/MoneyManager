0644b5febcd2963b06bd83b6d42b3c5ce12e53a7
dieudo25 <twahirwa25@gmail.com>
2024-10-13 12:59:51 +0200

    Add CategoryService.Domain & Common.Models

---
+++ CategoryService.Domain/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using Common.Domain.Enums;
+using Newtonsoft.Json;
+
+namespace CategoryService.Domain.Models
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public TransactionTypeEnum TransactionType { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}
--- Common.Domain/Enums/TransactionTypeEnum.cs
+++ Common.Domain/Enums/TransactionTypeEnum.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TransactionService.Domain.Models
+namespace Common.Domain.Enums
 {
     public enum TransactionTypeEnum
     {
